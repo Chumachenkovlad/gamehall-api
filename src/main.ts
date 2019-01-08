d4783ec8fdc8e18410fca9ca4ac3047dbb74216f
Chumachenkovlad <chumachenko.vla@gmail.com>
2019-01-08 21:23:20 +0200

    added cors for deveopment mode

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ErrorsInterceptor());
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors({
+      origin: 'http://localhost:4200'
+    });
+  }
+
   await app.listen(3000);
 }
 bootstrap();
